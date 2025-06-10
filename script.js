fa81586e660f7144dcbffb55744b2acd178f115f
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-06-10 17:25:53 +0530

    Updated animation

--- script.js
+++ script.js
@@ -7,36 +7,20 @@ menuButton.addEventListener('click', handleMenu)
 menu.addEventListener('click', handleMenu)
 
 function handleMenu() {
-    isOpen = !isOpen
+  isOpen = !isOpen
 
-    menu.style.display = isOpen ? 'flex' : 'none'
+  menu.style.display = isOpen ? 'flex' : 'none'
 
-    iconMenu[0].style.display = isOpen ? 'none' : 'flex'
-    iconMenu[1].style.display = isOpen ? 'flex' : 'none'
+  iconMenu[0].style.display = isOpen ? 'none' : 'flex'
+  iconMenu[1].style.display = isOpen ? 'flex' : 'none'
 }
 
-// Base config (optional if you set per element)
-const sr = ScrollReveal({
+ScrollReveal({
   reset: true,
   distance: '100px',
   duration: 1000,
   delay: 200,
 });
 
-// Reveal profile image from top
-sr.reveal('.profile', {
-  origin: 'top',
-});
-
-// Reveal Download CV from left
-sr.reveal('.download-cv', {
-  origin: 'left',
-  viewFactor: 0.1
-});
-
-// Reveal Contact Info from right
-sr.reveal('.contact', {
-  origin: 'right',
-});
-
-ScrollReveal().reveal('.home .contact', { origin: 'right' })
+ScrollReveal().reveal('.profile, .about ', { origin: 'top'});
+ScrollReveal().reveal('.home .home-button', {origin: 'bottom'})
